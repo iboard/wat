775eab648b6d58fb2688462f6b404df883187f13
Andi Altendorfer <andi@iboard.cc>
2012-03-09 19:41:27 +0100

    Add omniauth.rb since secrets are in secret.yml

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,23 @@
+require 'openid/store/filesystem'
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+
+  configure do |config|
+    config.path_prefix = '/auth' if RAILS_ENV == 'production'
+  end
+
+
+  Secrets::secret['omniauth'].each do |service, definition|
+    provider service.to_sym, definition['key'], definition['secret']
+  end
+
+  if Secrets::secret['openid']
+    _store = File::join(Rails.root, Secrets::secret['openid']['store'])
+    provider :openid, :store => OpenID::Store::Filesystem.new(_store)
+  end
+
+  provider :identity, :fields => [:name], :auth_key => 'name', on_failed_registration: lambda { |env|    
+    IdentitiesController.action(:new).call(env)
+  }
+
+end
