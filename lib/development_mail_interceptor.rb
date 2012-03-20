715411ad54aadee22dc611962241c50f542ce8d3
Andi Altendorfer <andi@iboard.cc>
2012-03-20 16:02:06 +0100

    Config developer-address in settings

--- lib/development_mail_interceptor.rb
+++ lib/development_mail_interceptor.rb
@@ -1,6 +1,6 @@
 class DevelopmentMailInterceptor
   def self.delivering_email(message)
     message.subject = "[#{message.to}] #{message.subject}"
-    message.to = "andi@iboard.cc"
+    message.to = Settings.developer_mail_address
   end
 end
