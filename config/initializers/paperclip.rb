acda2323707681d7cb21318c0d0f53d3ae30c978
Andi Altendorfer <andi@iboard.cc>
2012-04-16 06:56:09 +0200

    Make path for imagemagick configurable

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,5 +3,8 @@
 # class ActionDispatch::Http::UploadedFile
 #   include Paperclip::Upfile
 # end
+if _path=Settings.image_magick_path
+  Paperclip.options[:image_magick_path] = _path
+end
 
 require File::dirname(__FILE__) + "/../../lib/paperclip_processors/cropper.rb"
