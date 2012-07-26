9ea8ab7edac5a121160a47f3d76e2f2ea8382591
Andi Altendorfer <andi@iboard.cc>
2012-07-26 03:09:16 +0200

    Added Attachment- and UserAttachmentPresenter
    attachment_presenter:
    
      Base-class for all Attachement-derivates
    
    user_attachment_present:
    
      - derives from AttachmentPresenter
      - arranges Link, Image/Video/Audio (if that type), file-info
    
    _attachment.haml:
    
      - cleaned up

---
+++ app/presenters/attachment_presenter.rb
@@ -0,0 +1,37 @@
+class AttachmentPresenter < BasePresenter
+
+private
+  def insert_tag(attachment)
+    if attachment.file.content_type =~ /image/
+      content_tag :div do
+        newline + image_tag( attachment.application_file.file.url(:small) )
+      end
+    elsif attachment.file.content_type =~ /video/
+      content_tag :div do
+        newline + video_tag( attachment.file.url, :size => "320x240", controls: true )
+      end
+    elsif attachment.file.content_type =~ /audio\/mp3/
+      content_tag :div do
+        newline + audio_tag( attachment.file.url, controls: true )
+      end
+    else
+      ""
+    end
+  end
+
+  def newline
+    "<br/>".html_safe
+  end
+
+  def file_name_link(attachment)
+    content_tag :span, class: 'file-name-link' do
+      icon_link_to 'icon-download','stamped', attachment.file_name, attachment.file.url(:original), target: :blank
+    end
+  end
+
+  def file_info(attachment)
+    content_tag :span, class: 'file-info' do
+      attachment.file.content_type + ", " + humanized_filesize(attachment.file.size)
+    end
+  end
+end
