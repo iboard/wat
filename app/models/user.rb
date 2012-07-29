01d06ea848dbc88c132ff642352172208fa19f15
Andi Altendorfer <andi@iboard.cc>
2012-07-30 00:05:44 +0200

    set default location of user for edit.haml by an virtual-attribute-default

--- app/models/user.rb
+++ app/models/user.rb
@@ -225,6 +225,8 @@ class User
   def location_token
     if self.location && (self.location['lat'].present? && self.location['lng'].present?)
       "%3.4f,%3.4f" % [self.location['lat'], self.location['lng']]
+    else
+      "51.4771,0" # Greenwich Park, London
     end
   end
 
