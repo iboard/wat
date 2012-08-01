1b1e82f12502c1e51cc45254443b234c30e75428
Andi Altendorfer <andi@iboard.cc>
2012-08-01 19:31:24 +0200

    re-merge from campus

--- app/presenters/page_presenter.rb
+++ app/presenters/page_presenter.rb
@@ -3,7 +3,7 @@ class PagePresenter < BasePresenter
   presents :page
 
   def title
-    page.title
+    page.title || translate_link
   end
 
   def created_at
@@ -11,17 +11,22 @@ class PagePresenter < BasePresenter
   end
 
   def body
-    interpret page.body
+    interpret (page.body||translate_link)
   end
 
   def body_snippet body_snippet
-    interpret body_snippet
+    interpret( body_snippet || translate_link )
   end
 
   def banner_preview
     if page.banner_exist?
       image_tag page.banner.banner.url(:preview), class: 'pull-right banner-preview' 
     end
+  end
+
+private
+  def translate_link
+    link_to( t(:please_translate_to, :lang => I18n.locale), edit_page_path(page) )
   end
 
 end
