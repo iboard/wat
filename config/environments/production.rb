aa477d85b93c520bda4fdec232fcfe7b3aff52d3
Andi Altendorfer <andi@iboard.cc>
2012-08-04 18:53:33 +0200

    Add CKEdit.css to asset-pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,7 @@ Wat::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += ['ckeditor/*'] 
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
