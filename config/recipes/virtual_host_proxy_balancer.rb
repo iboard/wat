da2c59353d9c13cedcce307b85ce588d932bee7c
Andi Altendorfer <andi@iboard.cc>
2012-09-12 21:01:45 +0200

    Add recipes for apache-proxy

--- config/recipes/virtual_host_proxy_balancer.rb
+++ config/recipes/virtual_host_proxy_balancer.rb
@@ -1,12 +1,25 @@
-namespace :virtual_host_proxy_balancer do
+namespace :deploy do
 
   desc "Enable neccesary apache-modules on the server"
-  task :install, roles: :web do
+  task :install_proxy_module, roles: :web do
+    run "#{sudo} a2enmod proxy"
     run "#{sudo} a2enmod proxy_balancer"
     run "#{sudo} a2enmod rewrite"
   end
-  after "deploy:install", "virtual_host_proxy_balancer:install"
-  
+  after "deploy:install", "deploy:install_proxy_module"
+
+  desc "Install proxy-configs"
+  task :install_proxy_configs, roles: :web do
+    %w( proxy_balancer.load proxy.conf proxy_http.load proxy.load ).each do |config|
+      template "apache_#{config}.erb", "/tmp/apache_#{config}"
+      run "#{sudo} mv /tmp/apache_#{config} /etc/apache2/mods-available/#{config}"
+      run "#{sudo} [ -f /etc/apache2/mods-enabled/#{config} ] && #{sudo} rm /etc/apache2/mods-enabled/#{config}"
+      run "#{sudo} ln -s /etc/apache2/mods-available/#{config} /etc/apache2/mods-enabled/#{config}"
+    end
+    run "#{sudo} apache2ctl graceful"
+  end
+  after "deploy:install_proxy_module", "deploy:install_proxy_configs"
+
 
   desc "Enable a virtual host for the application and restart apache"
   task :setup, roles: :web do
