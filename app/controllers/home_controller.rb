c9d5efd362cb82b51c5155e4abc5361278cea84b
Karl Hochfilzer <karl.hochfilzer@wwedu.com>
2012-10-19 10:32:11 +0200

    Restore missmach cleanup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,7 @@
 # -*- encoding : utf-8 -*-
 class HomeController < ApplicationController
-  include Redirect::RootPath
   
-  # def index
-  def home_index
-    Rails.logger.info("HomeController index: params? #{params.inspect} 
-      session? #{session.inspect} 
-      current_user? #{current_user.inspect}")
+  def index
     if can_read?('Admin', 'Maintainer')
       @hero = Page.where(_id: 'hero').first
       @features = Page.featured
--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Wat::Application.initialize!
-# HomeController.send(:include, Redirect::RootPath)
