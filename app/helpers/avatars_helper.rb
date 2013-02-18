808bf2bc4d5fccbe56deb181fc4896e1b1e92146
Karl Hochfilzer <karl.hochfilzer@wwedu.com>
2013-02-18 14:58:26 +0100

    troubles with avatar, forgotten avatars-helper

--- app/helpers/avatars_helper.rb
+++ app/helpers/avatars_helper.rb
@@ -5,6 +5,7 @@ module AvatarsHelper
   def avatar(size)
     return "".html_safe unless current_user
     rc=""
+    current_user.reload
     present current_user do |p|
       rc = p.avatar(size)
     end
